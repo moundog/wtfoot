cc293ef60b0e7c160b14ede4f6762e8fc8b58e18
Mounir Krichane <mounir.krichane@rts.ch>
2016-03-23 12:05:19 +0100

    Integrated the points

--- js/Hackaton.js
+++ js/Hackaton.js
@@ -1,5 +1,15 @@
 var actualElemPos = 0;
 var isFinish = false;
+var p = ["Eden Hazard", "Petr Čech"];
+var a = [];
+var j = [];
+j[0] = [1, 1, 3];
+j[1] = [3, 1, 5];
+var r = [];
+for (var l = 0; l < j.length; l++) {
+    r[l] = [];
+}
+var t = [];
 
 window.onload = function () {
     $('.question').hide();
@@ -14,6 +24,7 @@ function nextElement() {
     if ($('.question').length - 1 < actualElemPos) {
         alert("Finish");
         isFinish = true;
+        getResult();
         return;
     }
     setTimeout( showElem, 500 );
@@ -23,4 +34,34 @@ function showElem() {
     $('.question:eq(' + actualElemPos + ')').fadeIn(500);
     if ($('.question:eq(' + actualElemPos + ')').attr('id') == "goal-game")
         startGoalGame();
+}
+
+function storeAnswer(e) {
+    a.push($(e).attr("value"));
+    nextElement();
+}
+
+function storeAnswerStratGame(v) {
+    a.push(v);
+    nextElement();
+}
+
+function storeAnswerGoal(v) {
+    a.push(v);
+    nextElement();
+}
+
+function getResult() {
+    for (var i = 0; i < j.length; i++) {
+        for (var k = 0; k < a.length; k++) {
+            r[i][k] = Math.abs(a[i] - j[i][k]);
+        }
+        var m = 0;
+        $.each(r[i], function() {
+            m += this;
+        });
+        t.push(m);
+    }
+    var mm = t.indexOf(Math.min.apply(Math, t));
+    console.log(p[mm]);
 }
